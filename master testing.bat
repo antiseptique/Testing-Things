a968d3666fb4a6e70de71281b3c38c90af82fd0f
antiseptique <taraxkawai@gmail.com>
2018-05-25 23:22:19 +0200

    First push
    Co-Authored-By: bibifok <bibifok@users.noreply.github.com>

---
+++ master testing.bat
@@ -0,0 +1,24 @@
+@echo off
+:setpcname
+cls
+echo Nom actuel du PC:
+hostname
+@echo.
+set /p pcnewname=Saisir le nouveau nom du PC:
+@echo.
+
+set nameincrementation=%COMPUTERNAME%%pcnewname%
+wmic computersystem where name="%COMPUTERNAME%" call rename name="%nameincrementation%"
+echo Le nouveau nom du PC est "%nameincrementation%"
+@echo.
+
+:rebootconfirmation
+set /p rebootvalidation=Confimez-vous le nouveau nom (Entraine le reboot) (y/n):
+
+if /i "%rebootvalidation%" EQU "Y" goto :rebootok
+if /i "%rebootvalidation%" EQU "N" goto :setpcname
+goto :rebootconfirmation
+
+:rebootok
+shutdown -r -t 10
+del %0
